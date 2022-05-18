7ba9257cd86f3a7fc8c4082667d763d67103da72
bakm04 <mike.baker@csgi.com>
2022-05-18 06:44:59 -0500

    Add Difficulty enum to domain and update recipe.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -19,8 +19,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //  TODO:  Add
-    // private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @Lob
     private Byte[] image;
 
@@ -113,5 +113,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
